e54fb99578015263a3911341bc482d893da31e1b
Mikaela Lammers <miklammers12@gmail.com>
2024-01-11 13:12:54 -0600

    Creating react and importing app

---
+++ src/Weather.js
@@ -0,0 +1,64 @@
+import React {useState} from "react";
+import "./styles.css";
+import axios from "axios";
+
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
+  return (
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <h2>Last updated: {weatherData.date}</h2>
+      </div>
+      <div className="row">
+        <div className="col-8">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <strong>{weatherData.temperature} </strong>
+            <span className="units">
+              <a href="/">°C</a> | <a href="/">°F</a>
+            </span>
+            <div className="description">{weatherData.description}</div>
+          </div>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
