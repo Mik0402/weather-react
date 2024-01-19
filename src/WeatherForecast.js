0975c35af84d424726cdb103f790e9b84de10a2a
Mikaela Lammers <miklammers12@gmail.com>
2024-01-19 17:09:06 -0600

    Added API forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,35 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
-import Axios from "axios";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
-
-    let apiKey= "fb9ba2ba068d2c8faabc8f622ed7013b";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`;
     
-    Axios.get(apiURL).then(handleResponse);
-
-    return (
+    if (loaded) {
+        return (
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col">
-                    <div className="WeatherForecast-day">Thu</div>
-                    <WeatherIcon code="01d" size={36} />
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">19º</span>
-                        <span className="WeatherForecast-temperature-min">10º</span>
-                    </div>
+                    <WeatherForecastDay data={forecast[0]} />
                 </div>
             </div>
         </div>
     );
+    } else {
+    let apiKey= "fb9ba2ba068d2c8faabc8f622ed7013b";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        
+        axios.get(apiURL).then(handleResponse);
+
+        return null;
+    }
 }
