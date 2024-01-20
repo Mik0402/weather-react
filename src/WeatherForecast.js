1f7758e532c5ec58b6bd7718d68a5409c9838347
Mikaela Lammers <miklammers12@gmail.com>
2024-01-19 18:37:58 -0600

    Creating loop and useEffect to update forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay.js";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
 
     function handleResponse(response) {
         setForecast(response.data.daily);
@@ -16,21 +20,15 @@ export default function WeatherForecast(props) {
         return (
         <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <WeatherForecastDay data={forecast[1]} />
-                </div>
-                <div className="col">
-                    <WeatherForecastDay data={forecast[2]} />
-                </div>
-                <div className="col">
-                    <WeatherForecastDay data={forecast[3]} />
-                </div>
-                <div className="col">
-                    <WeatherForecastDay data={forecast[4]} />
-                </div>
-                <div className="col">
-                    <WeatherForecastDay data={forecast[5]} />
-                </div>
+                {forecast.map(function (dailyForecast, index){
+                    if (index < 6) {
+                    return (
+                    <div className="col">
+                    <WeatherForecastDay data={dailyForecast} />
+                    </div>
+                );
+                }
+                })} 
             </div>
         </div>
     );
@@ -38,7 +36,7 @@ export default function WeatherForecast(props) {
         let apiKey= "0efb4fc16a9ed98dc0b3aafd8491d6ad";
         let longitude = props.coordinates.lon;
         let latitude = props.coordinates.lat;
-        let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
         
         axios.get(apiURL).then(handleResponse);
 
